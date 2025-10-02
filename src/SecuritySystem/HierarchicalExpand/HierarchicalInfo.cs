364509f224f4775aa2bdb1873fa5f96404a913f0
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-10-02 23:05:59 +0200

    upd HierarchicalInfo (#9)

--- src/SecuritySystem/HierarchicalExpand/HierarchicalInfo.cs
+++ src/SecuritySystem/HierarchicalExpand/HierarchicalInfo.cs
@@ -19,14 +19,14 @@ public abstract record HierarchicalInfo<TDomainObject, TDirectedLink>(
 
 public abstract record HierarchicalInfo<TDomainObject>(Expression<Func<TDomainObject, TDomainObject?>> ParentPath) : HierarchicalInfo
 {
-    //public override Type DomainObjectType { get; } = domainType;
+    public override Type DomainObjectType { get; } = typeof(TDomainObject);
 
     public Func<TDomainObject, TDomainObject?> ParentFunc { get; } = ParentPath.Compile();
 }
 
 public abstract record HierarchicalInfo
 {
-    //public abstract Type DomainObjectType { get; }
+    public abstract Type DomainObjectType { get; }
 
     public abstract Type DirectedLinkType { get; }
 
