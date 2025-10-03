0f8b3253fe8f69cae09ff21a5067d91b6463a824
Atsuta, Ivan <IAtsuta@luxoft.com>
2025-10-03 19:51:19 +0200

    upd

--- src/SecuritySystem/HierarchicalExpand/HierarchicalInfo.cs
+++ src/SecuritySystem/HierarchicalExpand/HierarchicalInfo.cs
@@ -16,19 +16,3 @@ public abstract record HierarchicalInfo<TDomainObject, TDirectedLink>(
 {
     public override Type DirectedLinkType { get; } = typeof(TDirectedLink);
 }
-
-public abstract record HierarchicalInfo<TDomainObject>(Expression<Func<TDomainObject, TDomainObject?>> ParentPath) : HierarchicalInfo
-{
-    public override Type DomainObjectType { get; } = typeof(TDomainObject);
-
-    public Func<TDomainObject, TDomainObject?> ParentFunc { get; } = ParentPath.Compile();
-}
-
-public abstract record HierarchicalInfo
-{
-    public abstract Type DomainObjectType { get; }
-
-    public abstract Type DirectedLinkType { get; }
-
-    public abstract Type UndirectedLinkType { get; }
-}
