0286831e61ab141df43b1ca1b1c3037421bbd63b
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-11-23 22:50:07 +0100

    upd GenericQueryable (#26)

--- src/SecuritySystem/HierarchicalExpand/DomainObjectExpander.cs
+++ src/SecuritySystem/HierarchicalExpand/DomainObjectExpander.cs
@@ -7,42 +7,38 @@ using SecuritySystem.Services;
 namespace SecuritySystem.HierarchicalExpand;
 
 public class DomainObjectExpander<TDomainObject>(HierarchicalInfo<TDomainObject> hierarchicalInfo, IQueryableSource queryableSource)
-    : IDomainObjectExpander<TDomainObject>
-    where TDomainObject : class
+	: IDomainObjectExpander<TDomainObject>
+	where TDomainObject : class
 {
-    public async Task<IEnumerable<TDomainObject>> GetAllParents(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
-    {
-        var allResult = startDomainObjects.ToHashSet();
+	public async Task<IEnumerable<TDomainObject>> GetAllParents(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
+	{
+		var allResult = startDomainObjects.ToHashSet();
 
-        for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
-        {
-            var preNextLayer = await queryableSource.GetQueryable<TDomainObject>()
-                .Where(currentObj => nextLayer.Contains(currentObj))
-                .Select(hierarchicalInfo.ParentPath)
-                .Where(nextObj => nextObj != null && !allResult.Contains(nextObj))
-                .Select(nextObj => nextObj!)
-                .GenericToListAsync(cancellationToken);
+		for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
+		{
+			nextLayer = await queryableSource.GetQueryable<TDomainObject>()
+				.Where(currentObj => nextLayer.Contains(currentObj))
+				.Select(hierarchicalInfo.ParentPath)
+				.Where(nextObj => nextObj != null && !allResult.Contains(nextObj))
+				.Select(nextObj => nextObj!)
+				.GenericToHashSetAsync(cancellationToken);
+		}
 
-            nextLayer = preNextLayer.ToHashSet();
-        }
+		return allResult;
+	}
 
-        return allResult;
-    }
+	public async Task<IEnumerable<TDomainObject>> GetAllChildren(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
+	{
+		var allResult = startDomainObjects.ToHashSet();
 
-    public async Task<IEnumerable<TDomainObject>> GetAllChildren(IEnumerable<TDomainObject> startDomainObjects, CancellationToken cancellationToken)
-    {
-        var allResult = startDomainObjects.ToHashSet();
+		for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
+		{
+			nextLayer = await queryableSource.GetQueryable<TDomainObject>()
+				.Where(hierarchicalInfo.ParentPath.Select(parentRef => parentRef != null && nextLayer.Contains(parentRef)))
+				.Where(nextObj => !allResult.Contains(nextObj))
+				.GenericToHashSetAsync(cancellationToken);
+		}
 
-        for (var nextLayer = allResult; nextLayer.Any(); allResult.UnionWith(nextLayer))
-        {
-            var preNextLayer = await queryableSource.GetQueryable<TDomainObject>()
-                .Where(hierarchicalInfo.ParentPath.Select(parentRef => parentRef != null && nextLayer.Contains(parentRef)))
-                .Where(nextObj => !allResult.Contains(nextObj))
-                .GenericToListAsync(cancellationToken);
-
-            nextLayer = preNextLayer.ToHashSet();
-        }
-
-        return allResult;
-    }
+		return allResult;
+	}
 }
