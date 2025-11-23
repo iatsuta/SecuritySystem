29c81887855ce79bdc672b04dc5272ff8c54a00f
iatsuta <terraghost@mail.ru>
2025-11-23 01:50:57 +0100

    upd GenericQueryable

--- src/SecuritySystem.VirtualPermission/SecuritySystemSettingsExtensions.cs
+++ src/SecuritySystem.VirtualPermission/SecuritySystemSettingsExtensions.cs
@@ -10,37 +10,36 @@ namespace SecuritySystem.VirtualPermission;
 
 public static class SecuritySystemSettingsExtensions
 {
-    public static ISecuritySystemSettings AddVirtualPermission<TPrincipal, TPermission>(
-        this ISecuritySystemSettings securitySystemSettings,
-        VirtualPermissionBindingInfo<TPrincipal, TPermission> bindingInfo)
-        where TPrincipal : class
-        where TPermission : class =>
-        securitySystemSettings
-
-            .AddPermissionSystem(
-                sp => ActivatorUtilities.CreateInstance<VirtualPermissionSystemFactory<TPrincipal, TPermission>>(sp, bindingInfo))
-
-            .AddExtensions(
-                sc => sc.AddScoped<IPrincipalSourceService>(
-                            sp => ActivatorUtilities.CreateInstance<VirtualPrincipalSourceService<TPrincipal, TPermission>>(
-                                sp,
-                                bindingInfo))
-
-                        .TryAddSingleton<IVirtualPermissionBindingInfoValidator, VirtualPermissionBindingInfoValidator>());
-
-    public static ISecuritySystemSettings AddVirtualPermission<TPrincipal, TPermission>(
-        this ISecuritySystemSettings securitySystemSettings,
-        SecurityRole securityRole,
-        Expression<Func<TPermission, TPrincipal>> principalPath,
-        Expression<Func<TPrincipal, string>> principalNamePath,
-        Func<VirtualPermissionBindingInfo<TPrincipal, TPermission>, VirtualPermissionBindingInfo<TPrincipal, TPermission>>? initFunc = null)
-        where TPrincipal : class
-        where TPermission : class
+    extension(ISecuritySystemSettings securitySystemSettings)
     {
-        var bindingInfo =
-            (initFunc ?? (v => v)).Invoke(
-                new VirtualPermissionBindingInfo<TPrincipal, TPermission>(securityRole, principalPath, principalNamePath));
+	    public ISecuritySystemSettings AddVirtualPermission<TPrincipal, TPermission>(VirtualPermissionBindingInfo<TPrincipal, TPermission> bindingInfo)
+		    where TPrincipal : class
+		    where TPermission : class =>
+		    securitySystemSettings
 
-        return securitySystemSettings.AddVirtualPermission(bindingInfo);
+			    .AddPermissionSystem(
+				    sp => ActivatorUtilities.CreateInstance<VirtualPermissionSystemFactory<TPrincipal, TPermission>>(sp, bindingInfo))
+
+			    .AddExtensions(
+				    sc => sc.AddScoped<IPrincipalSourceService>(
+						    sp => ActivatorUtilities.CreateInstance<VirtualPrincipalSourceService<TPrincipal, TPermission>>(
+							    sp,
+							    bindingInfo))
+
+					    .TryAddSingleton<IVirtualPermissionBindingInfoValidator, VirtualPermissionBindingInfoValidator>());
+
+	    public ISecuritySystemSettings AddVirtualPermission<TPrincipal, TPermission>(SecurityRole securityRole,
+		    Expression<Func<TPermission, TPrincipal>> principalPath,
+		    Expression<Func<TPrincipal, string>> principalNamePath,
+		    Func<VirtualPermissionBindingInfo<TPrincipal, TPermission>, VirtualPermissionBindingInfo<TPrincipal, TPermission>>? initFunc = null)
+		    where TPrincipal : class
+		    where TPermission : class
+	    {
+		    var bindingInfo =
+			    (initFunc ?? (v => v)).Invoke(
+				    new VirtualPermissionBindingInfo<TPrincipal, TPermission>(securityRole, principalPath, principalNamePath));
+
+		    return securitySystemSettings.AddVirtualPermission(bindingInfo);
+	    }
     }
 }
