6210f044466d7fbd3b15607d941526cc8b72863e
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-01-05 13:43:58 +0100

    fix UserCredentialMatcher (#45)

---
+++ src/_Example/ExampleApp.IntegrationTests/RunAsTests.cs
@@ -0,0 +1,67 @@
+﻿using ExampleApp.Domain.Auth.General;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using SecuritySystem;
+using SecuritySystem.Services;
+using SecuritySystem.Testing;
+using SecuritySystem.UserSource;
+
+namespace ExampleApp.IntegrationTests;
+
+public class RunAsTests : TestBase
+{
+    [Fact]
+    public async Task StartRunAsUser_AssignsRunAsPrincipalToCurrentUser()
+    {
+        // Arrange
+        await this.AuthManager.For().CreatePrincipalAsync(this.CancellationToken);
+
+        var runAsUserName = nameof(RunAsTests);
+        var runAsUserIdentity = await this.AuthManager.For(runAsUserName).CreatePrincipalAsync(this.CancellationToken);
+        var runAsUserId = (Guid)runAsUserIdentity.GetId();
+
+        // Act
+        await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<IRunAsManager>>().EvaluateAsync(TestingScopeMode.Write, manager =>
+            manager.StartRunAsUserAsync(runAsUserIdentity, this.CancellationToken));
+
+        // Assert
+        var currentUserName = await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<ICurrentUser>>()
+            .EvaluateAsync(TestingScopeMode.Read, async c => c.Name);
+
+        var currentUserId = await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<ICurrentUserSource<Principal>>>()
+            .EvaluateAsync(TestingScopeMode.Read, async c => c.CurrentUser.Id);
+
+        currentUserName.Should().Be(runAsUserName);
+        currentUserId.Should().Be(runAsUserId);
+    }
+
+
+    [Fact]
+    public async Task StartRunAsUser_WhenAlreadyRunningAsUser_DoesNotChangeRunAs()
+    {
+        // Arrange
+        await this.AuthManager.For().CreatePrincipalAsync(this.CancellationToken);
+
+        var runAsUserName = nameof(RunAsTests);
+        var runAsUserIdentity = await this.AuthManager.For(runAsUserName).CreatePrincipalAsync(this.CancellationToken);
+        var runAsUserId = (Guid)runAsUserIdentity.GetId();
+
+        // Act
+        await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<IRunAsManager>>().EvaluateAsync(TestingScopeMode.Write, manager =>
+            manager.StartRunAsUserAsync(runAsUserIdentity, this.CancellationToken));
+
+        await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<IRunAsManager>>().EvaluateAsync(TestingScopeMode.Write, manager =>
+            manager.StartRunAsUserAsync(runAsUserIdentity, this.CancellationToken));
+
+        // Assert
+        var currentUserName = await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<ICurrentUser>>()
+            .EvaluateAsync(TestingScopeMode.Read, async c => c.Name);
+
+        var currentUserId = await this.RootServiceProvider.GetRequiredService<ITestingEvaluator<ICurrentUserSource<Principal>>>()
+            .EvaluateAsync(TestingScopeMode.Read, async c => c.CurrentUser.Id);
+
+        currentUserName.Should().Be(runAsUserName);
+        currentUserId.Should().Be(runAsUserId);
+    }
+}
